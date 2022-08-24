990b8690f84b8b59f3ed032d246508085cc55a16
stefanblockchain <crazyblockchain@gmail.com>
2022-08-25 00:38:57 +0200

    Updated test for WakandaBallot contract

--- smart_contracts/contracts/WakandaBallot.sol
+++ smart_contracts/contracts/WakandaBallot.sol
@@ -14,77 +14,86 @@ contract WakandaBallot is Ownable{
         uint256 count;
     }
 
+    struct Voter {
+        address voterAddress;
+        uint256 voteCount;
+        bytes32 candidate;
+    }
+
     enum VotingState {NOT_STARTED, STARTED, FINISHED}
 
     mapping(bytes32 => bool) registeredCandidates;
 
-    mapping(address => uint256) public voters;
+    mapping(address => uint256) public voted;
 
     Candidate[] public candidates;
 
     Candidate[] public winners;
 
+    Voter[] public voters;
+
     uint256 public startTime;
 
     uint256 public endTime;
+
+    uint256 totalVotes;
 
     IERC20 public wknd;
 
     event NewChallenger(string indexed name, string indexed cult, uint age);
 
+    event Voted(address indexed voter, uint256 voteCount, string name, string cult);
+
     constructor (uint256 _startTime, uint256 _endTime, address _wknd) {
+        require(_startTime > block.timestamp, 'Start time can not be in the past');
         require(_endTime > _startTime, 'End time must come after start time');
         require(_wknd != address(0), 'WKND token address cant be zero');
 
-        startTime = _startTime;
+        startTime =  _startTime;
         endTime = _endTime;
 
         wknd = IERC20(_wknd);
     }
 
-    modifier isNotStarted(){
-        require(getElectionState() == VotingState.NOT_STARTED, 'Voting is not in correct state');
+    modifier inState(VotingState state){
+        require(getElectionState() == state, 'Voting is not in correct state');
         _;
     }
 
-    modifier isStarted(){
-        require(getElectionState() == VotingState.STARTED, 'Voting is not started.');
-        _;
-    }
-
-    modifier isFinished(){
-        require(getElectionState() == VotingState.FINISHED, 'Voting is not finished.');
-        _;
-    }
-
-    function addCandidate(string memory name, string memory cult, uint age) public onlyOwner isNotStarted{
+    function addCandidate(string memory name, string memory cult, uint age) public onlyOwner inState(VotingState.NOT_STARTED){
         bytes32 _hash = keccak256(abi.encodePacked(name, cult, age));
-        require(registeredCandidates[_hash],'Candidate already added');
+        require(!registeredCandidates[_hash],'Candidate already added');
         candidates.push(Candidate(name, cult, age, _hash, uint(0)));
+        registeredCandidates[_hash] = true;
     }
 
-    function vote(bytes32 _candidate, uint256 _voteNumber) public isStarted {
+    function vote(bytes32 _candidate, uint256 _voteNumber) public inState(VotingState.STARTED) {
         address sender = msg.sender;
-        require(voters[sender] == uint256(0), 'Already voted');
+        require(voted[sender] == uint256(0), 'Already voted');
         require(registeredCandidates[_candidate],'Not a registered candidate');
         require(_voteNumber > uint256(0),'Wrong vote count number');
         require(wknd.balanceOf(sender) >= _voteNumber, 'Vote number is bigger then ammount of WKND tokens owned for given address');
 
-        voters[sender] = _voteNumber;
         wknd.transferFrom(sender, address(this), _voteNumber);
+        voted[sender] = _voteNumber;
         uint256 _index = findCandidate(_candidate);
 
-        candidates[_index].count += 1;
+        candidates[_index].count += _voteNumber;
+        voters.push(Voter(sender, _voteNumber, _candidate));
+        totalVotes += _voteNumber;
 
         _setUpWinner(candidates[_index]);
+
+        emit Voted(sender,_voteNumber, candidates[_index].name, candidates[_index].cult);
     }
 
-    function unstake() public isFinished {
-        require(voters[msg.sender] > 0, 'Voter didnt stake any tokens');
+    function unstake() public inState(VotingState.FINISHED) {
+        require(voted[msg.sender] > 0, 'Voter didnt stake any tokens');
         address sender = msg.sender;
-        uint256 _balance = voters[sender];
-        voters[sender] = 0;
+        uint256 _balance = voted[sender];
+        voted[sender] = 0;
 
+        wknd.approve(address(this), _balance);
         wknd.transferFrom(address(this), sender, _balance);
     }
 
@@ -114,8 +123,8 @@ contract WakandaBallot is Ownable{
 
         if(!elementFound) return;
 
-        for(uint i = index; i < winners.length;i++)
-            winners[i] = winners[i+1];
+        for(uint i = index; i < winners.length -1;i++)
+            winners[i+1] = winners[i];
         
         winners[index] = winn;
         winners.pop();
--- smart_contracts/test/WakandaBallot.ts
+++ smart_contracts/test/WakandaBallot.ts
@@ -1,7 +1,7 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import axios from 'axios';
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
 describe("WakandaBallot", function () {
   async function deployOneYearLockFixture() {
@@ -19,13 +19,15 @@ describe("WakandaBallot", function () {
     const wakandaBallot = await WakandaBallot.deploy(startTime, endTime, wknd.address);
 
     const listOfCandidates = await getCandidates();
-    // listOfCandidates.candidates.forEach(async (candidate) => insertCandidate(candidate.name, candidate.cult, candidate.age, wakandaBallot))
+
+   for(const candidate of listOfCandidates){
+    await insertCandidate(candidate.name, candidate.cult, candidate.age, wakandaBallot);
+   }
     
-
-    //mint tokens for addresses
     await wknd.connect(otherAccount).mintToken();
+    await wknd.connect(thirdAccount).mintToken();
 
-    return { wakandaBallot,wknd, startTime, endTime, owner, otherAccount, thirdAccount };
+    return { wakandaBallot,wknd, startTime, endTime, listOfCandidates, owner, otherAccount, thirdAccount };
   }
 
   describe("Deployment", function () {
@@ -49,12 +51,195 @@ describe("WakandaBallot", function () {
       });
   });
 
-  // describe("Voting", function () {
-  //   it("Should fail to vote", async function () {
-  //     const { wakandaBallot, startTime, endTime, otherAccount } = await loadFixture(deployOneYearLockFixture);
+  describe('Voting', function(){
+    it('Should fail to vote before start time', async function () {
+      const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+      const firstCandidate = await wakandaBallot.candidates(ethers.BigNumber.from("0"));
+      const userBalance = await wknd.balanceOf(otherAccount.address);
       
-  //   });
-  // });
+      await expect(wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, userBalance)).to.be.revertedWith('Voting is not in correct state');
+    });
+
+    it('Should fail to add existing candidate', async function () {
+      const { wakandaBallot, listOfCandidates } = await loadFixture(deployOneYearLockFixture);
+      const firstCandidate = listOfCandidates[0];
+      
+      await expect(wakandaBallot.addCandidate(firstCandidate.name, firstCandidate.cult, firstCandidate.age)).to.be.revertedWith('Candidate already added');
+    });
+
+    it('Should fail to add  candidate if not owner of the contract', async function () {
+      const { wakandaBallot, otherAccount, listOfCandidates } = await loadFixture(deployOneYearLockFixture);
+      const firstCandidate = listOfCandidates[0];
+      
+      await expect(wakandaBallot.connect(otherAccount).addCandidate(firstCandidate.name, firstCandidate.cult, firstCandidate.age)).to.be.revertedWith('Ownable: caller is not the owner');
+    });
+
+    it('Should get correct vote state', async function () {
+      const { wakandaBallot, startTime } = await loadFixture(deployOneYearLockFixture);
+      const ONE_DAY = 24 * 60 * 60;
+      const THREE_DAYS = 3 * 24 * 60 * 60;
+
+      let result = await wakandaBallot.getElectionState();
+      expect(await wakandaBallot.getElectionState()).to.equal(VotingStates.NOT_STARTED);
+
+      await increaseTime(ONE_DAY);
+      result = await wakandaBallot.getElectionState();
+      expect(await wakandaBallot.getElectionState()).to.equal(VotingStates.STARTED);
+
+      await increaseTime(THREE_DAYS);
+      result = await wakandaBallot.getElectionState();
+      expect(await wakandaBallot.getElectionState()).to.equal(VotingStates.FINISHED);
+      
+  });
+
+    it('Should vote successfully', async function () {
+      const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+      const ONE_DAY = 24 * 60 * 60;
+
+      const candidateId = ethers.BigNumber.from("0");
+      const firstCandidate = await wakandaBallot.candidates(candidateId);
+      const userBalance = await wknd.balanceOf(otherAccount.address);
+      await wknd.connect(otherAccount).approve(wakandaBallot.address, userBalance);
+      await increaseTime(ONE_DAY);
+      await wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, userBalance);
+
+      const result = await wakandaBallot.candidates(candidateId);
+
+      expect(result.count).to.equal(ethers.BigNumber.from('1'));
+  });
+
+  it('Should prevent multiple voting', async function () {
+    const { wakandaBallot, otherAccount, thirdAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+    const ONE_DAY = 24 * 60 * 60;
+
+    const candidateId = ethers.BigNumber.from("0");
+    const firstCandidate = await wakandaBallot.candidates(candidateId);
+    const userBalance = await wknd.balanceOf(otherAccount.address);
+    await wknd.connect(otherAccount).approve(wakandaBallot.address, userBalance);
+
+    await increaseTime(ONE_DAY);
+    await wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, userBalance);
+    
+    const thirdAccountAmount = await wknd.balanceOf(thirdAccount.address);
+    await wknd.connect(thirdAccount).transfer(otherAccount.address, thirdAccountAmount);
+
+    await expect(wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, thirdAccountAmount)).to.be.revertedWith('Already voted');    
+});
+
+it('Should reduce wknd user balance for number of casted votes', async function () {
+  const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+  const ONE_DAY = 24 * 60 * 60;
+
+  const firstCandidate = await wakandaBallot.candidates(ethers.BigNumber.from("0"));
+  let balance = await wknd.balanceOf(otherAccount.address);
+  await wknd.connect(otherAccount).approve(wakandaBallot.address, balance);
+
+  await increaseTime(ONE_DAY);
+  await wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, balance);
+  
+  balance = await wknd.balanceOf(otherAccount.address);
+
+  expect(await wknd.balanceOf(otherAccount.address)).to.equal(ethers.BigNumber.from('0'));    
+});
+
+it('Should return correct winners', async function () {
+  const { wakandaBallot, otherAccount, thirdAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+  const ONE_DAY = 24 * 60 * 60;
+
+  const firstCandidateId = ethers.BigNumber.from("0");
+  const secondCandidateId = ethers.BigNumber.from("1");
+  const firstCandidate = await wakandaBallot.candidates(firstCandidateId);
+  const secondCandidate = await wakandaBallot.candidates(secondCandidateId);
+
+  const firstUserBalance = await wknd.balanceOf(otherAccount.address);
+  const secondUserBalance = await wknd.balanceOf(thirdAccount.address);
+  await wknd.connect(otherAccount).approve(wakandaBallot.address, firstUserBalance);
+  await wknd.connect(thirdAccount).approve(wakandaBallot.address, secondUserBalance);
+
+  await increaseTime(ONE_DAY);
+
+  await wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, firstUserBalance);
+  await wakandaBallot.connect(thirdAccount).vote(secondCandidate.hash, secondUserBalance);
+
+  //get winners list
+  const winners = await wakandaBallot.winningCandidates();
+  const firstWinner = winners[0];
+  const secondWinner = winners[1]; 
+
+  expect(winners.length).to.equal(2);
+
+  compareCandidates(firstCandidate, firstWinner);
+  expect(firstWinner.count).to.equal(firstUserBalance);
+
+  compareCandidates(secondCandidate, secondWinner);
+  expect(secondWinner.count).to.equal(secondUserBalance);
+});
+
+it('Should emit Voted event', async function () {
+  const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+  const ONE_DAY = 24 * 60 * 60;
+
+  const firstCandidate = await wakandaBallot.candidates(ethers.BigNumber.from("0"));
+  const balance = await wknd.balanceOf(otherAccount.address);
+  
+  await wknd.connect(otherAccount).approve(wakandaBallot.address, balance);
+
+  await increaseTime(ONE_DAY);
+
+  await expect(wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, balance))
+              .to.emit(wakandaBallot, 'Voted')
+              .withArgs(otherAccount.address, balance, firstCandidate.name, firstCandidate.cult);
+
+});
+});
+
+describe('Unstaking', function(){
+  it('Should fail to unstake before end of voting time', async function () {
+    const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+    const ONE_DAY = 24 * 60 * 60;
+  
+    const firstCandidate = await wakandaBallot.candidates(ethers.BigNumber.from("0"));
+    let balance = await wknd.balanceOf(otherAccount.address);
+    await wknd.connect(otherAccount).approve(wakandaBallot.address, balance);
+  
+    await increaseTime(ONE_DAY);
+    await wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, balance);
+    
+    balance = await wknd.balanceOf(otherAccount.address);
+  
+    await expect(wakandaBallot.connect(otherAccount).unstake()).to.be.revertedWith('Voting is not in correct state');   
+  });
+
+  it('Should fail to unstake if user didn\'t vote', async function () {
+    const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+    const FOUR_DAY = 4 * 24 * 60 * 60;
+        
+    await increaseTime(FOUR_DAY);
+
+    await expect(wakandaBallot.connect(otherAccount).unstake()).to.be.revertedWith('Voter didnt stake any tokens');   
+  });
+
+  it('Should unstake successfully', async function () {
+    const { wakandaBallot, otherAccount, wknd } = await loadFixture(deployOneYearLockFixture);
+    const ONE_DAY =  24 * 60 * 60;
+    const FOUR_DAY = 4 * ONE_DAY;
+
+    const prevBalance = await wknd.balanceOf(otherAccount.address);
+    await wknd.connect(otherAccount).approve(wakandaBallot.address, prevBalance);
+    await increaseTime(ONE_DAY);
+
+    const firstCandidate = await wakandaBallot.candidates(ethers.BigNumber.from("0"));
+    await wakandaBallot.connect(otherAccount).vote(firstCandidate.hash, prevBalance);
+    
+    await increaseTime(FOUR_DAY);
+
+    await wakandaBallot.connect(otherAccount).unstake();
+  
+    const currentBalance = await wknd.balanceOf(otherAccount.address);
+
+    expect(prevBalance).to.equal(currentBalance)
+  });
+});
 });
 
 
@@ -67,10 +252,28 @@ interface CanidateResponse {
 }
 
 async function getCandidates(url:string = 'https://wakanda-task.3327.io/list'){
-  const listOfCandidates: CanidateResponse = (await axios.get(url)).data;
-  return listOfCandidates;
+  const listOfCandidates = (await axios.get(url)).data;
+  return listOfCandidates.candidates;
 }
 
 async function insertCandidate(name:string, cult:string, age:string, wakandaBallot:any){
   await wakandaBallot.addCandidate(name, cult, age);
+}
+
+
+async function increaseTime(timeInSeconds: number){
+  await ethers.provider.send('evm_increaseTime', [timeInSeconds]);
+  await network.provider.send("evm_mine");
+}
+
+function compareCandidates(firstCandidate: any, secondCandidate: any){
+  expect(firstCandidate.name).to.equal(secondCandidate.name);
+  expect(firstCandidate.cult).to.equal(secondCandidate.cult);
+  expect(firstCandidate.age).to.equal(secondCandidate.age);
+}
+
+enum VotingStates {
+  NOT_STARTED = 0,
+  STARTED = 1,
+  FINISHED = 2
 }
